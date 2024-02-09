259b20376df1bbd78dd8c3787623a6f92fdf6b81
Victoria <vika.200.viktoria@gmail.com>
2024-02-09 20:41:24 +0200

    Task 3 done.

--- src/tri_area.py
+++ src/tri_area.py
@@ -1,9 +1,9 @@
 def tri_area(base, height):
   # Type your code
   square_area = base * height/2
-  print(square_area)
+  #print(square_area)
   return square_area
 
-base=int(input("Enter the base: "))
-height=int(input("Enter the height: "))
-tri_area(base,height)
+#base=int(input("Enter the base: "))
+#height=int(input("Enter the height: "))
+#tri_area(base,height)
