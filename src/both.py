e96e451618c90b12f6751cad0ea6c465aa14d007
Victoria <vika.200.viktoria@gmail.com>
2024-02-09 21:15:16 +0200

    Task 5 done.

--- src/both.py
+++ src/both.py
@@ -1,3 +1,13 @@
 def both(number1, number2):
   # Type your code
-  return
+  if number1>0 and number2>0 :
+    return True
+  elif number1<0 and number2<0:
+    return True
+  elif number1==0 and number2==0:
+    return True
+  else:
+    return False
+
+#test
+#print(both(1,0))
